c17194836be7e148b7d611d367eccf47fab764f7
Tanveer Ahmed <A-tanveer@users.noreply.github.com>
2016-12-23 02:37:24 +0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,22 @@
 import sys
+
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog
-import MainUi, clients
-from PyQt5 import uic, QtWidgets
+
+import MainUi
+import clients
+import ctypes
+
+# http://stackoverflow.com/questions/1551605/how-to-set-applications-taskbar-icon-in-windows-7/1552105#1552105
+myappid = 'SUST.Project_150.Tanveer\'s Hotspot'
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
 # LandingPageUI, LandingPageBase = uic.loadUiType("clients.ui")
 # http://stackoverflow.com/questions/41150669/open-second-window-from-main-with-pyqt5-and-qt-designer
 
 
 class MainWindow (QMainWindow, MainUi.Ui_MainWindow):
+
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         MainUi.Ui_MainWindow.__init__(self)
@@ -32,5 +41,4 @@ if __name__ == '__main__':
 
     window = MainWindow()
     window.show()
-    # view_clients()
     sys.exit(app.exec_())
