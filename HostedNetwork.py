25c8b0195eca6a8b25711cc214dac57a4e7d6d96
Tanveer Ahmed <A-tanveer@users.noreply.github.com>
2016-12-23 02:36:57 +0600

    Update HostedNetwork.py

--- HostedNetwork.py
+++ HostedNetwork.py
@@ -1,23 +1,27 @@
 import os
+import time
 
 
 # create or change hosted network
 def hosted_network_create(ssid, key):
-    command = "netsh wlan set hostednetwork mode=allow ssid=\"" + ssid + "\" key=" + key
+    command = "netsh wlan set hostednetwork mode=allow ssid=\"" + ssid + "\" key=\"" + key + "\""
     print(command)
     os.system(command)
+    # time.sleep(0.25)
 
 
 # start hosted network
 def hosted_network_start():
     command = "netsh wlan start hostednetwork"
     os.system(command)
+    # time.sleep(0.25)
 
 
 # stop hosted network
 def hosted_network_stop():
     command = "netsh wlan stop hostednetwork"
     os.system(command)
+    # time.sleep(0.25)
 
 
 # checks if the machine supports hosted network
@@ -45,6 +49,7 @@ def check_status():
     data = data[11]
     data = data.replace(' ', '').replace('\n', '').split(':')
     return data[1]
+initialization_of_temp_txt = check_status()
 
 
 # returns hosted network name
@@ -53,7 +58,12 @@ def get_name():
     data = f.readlines()
     data = data[4]
     data = data.replace('  ', '').replace('\n', '').split(':')
-    return data[1].replace('"', '')
+    data = data[1]
+    if data.startswith(' "'):
+        data = data[2:len(data)-1]
+    elif data.startswith(' '):
+        data = data[1:]
+    return data
 
 
 def get_password():
@@ -65,26 +75,39 @@ def get_password():
     f = open('temp.pass', 'r')
     data = f.readlines()
     passwd = data[6].replace(' ', '').split(':')[1]
-    return passwd
+    return passwd.replace('\n', '').replace(' ', '')
 
 
 # returns number of connected clients
 def get_no_connected_client():
+    f = open('temp.txt', 'w')
+    command = "netsh wlan show hostednetwork"
+    out = os.popen(command).read()
+    f.write(out)
+    f.close()
     f = open('temp.txt', 'r')
     data = f.readlines()
-    data = data[15]
-    data = data.replace('  ', '').replace('\n', '').split(':')
-    # print(data)
-    return data[1].replace(' ', '')
+    if len(data) > 15:
+        data = data[15]
+        data = data.replace('  ', '').replace('\n', '').split(':')
+        # print(data)
+        return data[1].replace(' ', '')
+    else:
+        return 'None'
 
 
 # returns mac addresses from netsh wlan show hostednetwork
 def get_clients_mac(num):
     mac_list = []
+    f = open('temp.txt', 'w')
+    command = "netsh wlan show hostednetwork"
+    out = os.popen(command).read()
+    f.write(out)
+    f.close()
     f = open('temp.txt', 'r')
     data = f.readlines()
     end = 15 + int(num) + 1
-    print(data[15])
+    # print(data[15])
     for i in range(16, end):
         datax = data[i]
         datax = datax.replace('\n', '').split()
